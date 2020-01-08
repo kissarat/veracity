42cc4306b3b63cf30df48767991c5d6f4edfb9c0
Taras Labiak <kissarat@gmail.com>
2020-01-08 17:24:45 +0200

    + summary test

--- test/describe.spec.js
+++ test/describe.spec.js
@@ -2,6 +2,28 @@ const { equal, ok } = require('assert');
 const { isObject } = require('auxiliary');
 const describeSchema = require('../src/describe');
 
+const routes = [
+    {
+        method: ['GET', 'POST'],
+        path: '/users'
+    },
+    {
+        method: ['PUT'],
+        path: '/users'
+    }
+];
+
+const description = {
+    summary: '7b40e7ae-c45b-475e-9d6e-6c4f61a3f26d',
+}
+
+const eachSchema = (schema, cb) => Object.keys(schema).forEach(prefix => {
+    const routes = schema[prefix];
+    Object.keys(routes).forEach(method => {
+        cb(routes[method], method, prefix);
+    });
+})
+
 describe('describe', () => {
     it('empty', () => {
         const schema = describeSchema();
@@ -9,24 +31,19 @@ describe('describe', () => {
     });
 
     it('simple', () => {
-        const schema = describeSchema({
-            routes: [
-                {
-                    method: ['GET', 'POST'],
-                    path: '/users'
-                },
-                {
-                    method: ['PUT'],
-                    path: '/users'
-                }
-            ]
-        });
-        Object.keys(schema).forEach(prefix => {
-            const routes = schema[prefix];
-            Object.keys(routes).forEach(method => {
-                const route = routes[method];
-                ok(isObject(route));
-            });
-        });
-    })
+        eachSchema(describeSchema({ routes }), () => {});
+    });
+
+    it('summary', () => {
+        eachSchema(
+            describeSchema({ routes: routes.map(r => ({
+                    ...r,
+                    ...description
+                }))
+            }),
+            route => {
+                equal(typeof route.summary, 'string');
+            }
+        );
+    });
 });
